b6442b941466d72df027a9f2f47357686b514f63
tom_brandi <brandner.thomas@me.com>
2025-04-30 16:04:38 +0200

    Update

--- src/at/htl/klu/methods/Test.java
+++ src/at/htl/klu/methods/Test.java
@@ -1,67 +1,67 @@
 import java.util.Arrays;
 
 public class Test {
-  public static void main(String[] args) {
+    public static void main(String[] args) {
 
-    int min = HtlMath.getMin(5, 10);
-    System.out.println("Ausgabe der niedriegsten Zahl");
-    System.out.println(min);
+        int min = HtlMath.getMin(5, 10);
+        System.out.println("Ausgabe der niedriegsten Zahl");
+        System.out.println(min);
 
-    int max = HtlMath.getMax(5, 10);
-    System.out.println("Ausgabe der höchsten Zahl.");
-    System.out.println(max);
+        int max = HtlMath.getMax(5, 10);
+        System.out.println("Ausgabe der höchsten Zahl.");
+        System.out.println(max);
 
-    int sum = HtlMath.getSum(5, 10);
-    System.out.println("Ausgabe der Summe");
-    System.out.println(sum);
+        int sum = HtlMath.getSum(5, 10);
+        System.out.println("Ausgabe der Summe");
+        System.out.println(sum);
 
-    double getQuotient = HtlMath.getQuotient(100, 25);
-    System.out.println("Ausgabe des Quotienten einer Division");
-    System.out.println(getQuotient);
+        double getQuotient = HtlMath.getQuotient(100, 25);
+        System.out.println("Ausgabe des Quotienten einer Division");
+        System.out.println(getQuotient);
 
-    boolean isEven = HtlMath.isEven(10);
-    System.out.println("Augabe ob die Zahl Even oder Odd ist.");
-    System.out.println(isEven);
+        boolean isEven = HtlMath.isEven(10);
+        System.out.println("Augabe ob die Zahl Even oder Odd ist.");
+        System.out.println(isEven);
 
-    int rnd = HtlMath.getRandomInt(5, 10);
-    System.out.println("Gibt eine Random Zahl zwischen zwei Ganzzahlen aus. ");
-    System.out.println(rnd);
+        int rnd = HtlMath.getRandomInt(5, 10);
+        System.out.println("Gibt eine Random Zahl zwischen zwei Ganzzahlen aus. ");
+        System.out.println(rnd);
 
-    double rndTwo = HtlMath.getRandomDouble(5, 10);
-    System.out.println(
-        "Gibt eine Random Gleitkommezahl zwischen zwei Ganzzahlen aus");
-    System.out.printf("%.2f", rndTwo);
-    System.out.println();
+        double rndTwo = HtlMath.getRandomDouble(5, 10);
+        System.out.println(
+                "Gibt eine Random Gleitkommezahl zwischen zwei Ganzzahlen aus");
+        System.out.printf("%.2f", rndTwo);
+        System.out.println();
 
-    boolean isDividedBy = HtlMath.isADividedByB(6, 2);
-    System.out.println("Ausgabe ob a / b  keine Rest hat");
-    System.out.println(isDividedBy);
+        boolean isDividedBy = HtlMath.isADividedByB(6, 2);
+        System.out.println("Ausgabe ob a / b  keine Rest hat");
+        System.out.println(isDividedBy);
 
-    double[] arr = new double[5];
-    arr = SAM3Random.Sam3Arr();
-    System.out.println("Gibt Summe, Anzahl, Max, Min, Avg");
-    System.out.println(Arrays.toString(arr));
+        double[] arr = new double[5];
+        arr = SAM3Random.Sam3Arr();
+        System.out.println("Gibt Summe, Anzahl, Max, Min, Avg");
+        System.out.println(Arrays.toString(arr));
 
-    int rndAbs = Calculations.getAbsValue();
-    System.out.println("Gibt einen Random Abs Value aus Random Zahlen zurück");
-    System.out.println(rndAbs);
+        int rndAbs = Calculations.getAbsValue();
+        System.out.println("Gibt einen Random Abs Value aus Random Zahlen zurück");
+        System.out.println(rndAbs);
 
-    boolean isPal = HtlStringUtil.isPalindrome("hannaH");
-    System.out.println(isPal);
+        boolean isPal = HtlStringUtil.isPalindrome("hannaH");
+        System.out.println(isPal);
 
-    boolean isQuest = HtlStringUtil.isQuestion("Hallo ");
-    System.out.println(
-        "Gibt zürck ob der übergebeben String als letztes ein Fragezeichen "
-        + "hat");
-    System.out.println(isQuest);
+        boolean isQuest = HtlStringUtil.isQuestion("Hallo ");
+        System.out.println(
+                "Gibt zürck ob der übergebeben String als letztes ein Fragezeichen "
+                        + "hat");
+        System.out.println(isQuest);
 
-    boolean isShout = HtlStringUtil.isRequest("Meine Hand ist ausgestreckt");
-    System.out.println(isShout);
+        boolean isShout = HtlStringUtil.isRequest("Meine Hand ist ausgestreckt");
+        System.out.println(isShout);
 
-    boolean isState = HtlStringUtil.isStatement("Hallo.");
-    System.out.println(isState);
+        boolean isState = HtlStringUtil.isStatement("Hallo.");
+        System.out.println(isState);
 
-    int a = HtlStringUtil.getOccurrences("Heute ist ein guter Tag.", 'e');
-    System.out.println("De Buchstaben a kommt " + a + "mal vor");
-  }
+        int a = HtlStringUtil.getOccurrences("Heute ist ein guter Tag.", 'e');
+        System.out.println("De Buchstaben a kommt " + a + "mal vor");
+    }
 }
--- src/at/htl/klu/methods/staticMethodenUbung.java
+++ src/at/htl/klu/methods/staticMethodenUbung.java
@@ -6,37 +6,39 @@ public class staticMethodenUbung {
 
   public static void main(String[] args) {
     System.out.println("------------------------");
-/*   int count = countNumerics("Es waren 17 Zwerge!");
-    System.out.println(count);
- */   int sum = solve("4");
+    /*   int count = countNumerics("Es waren 17 Zwerge!");
+        System.out.println(count);
+     */
+    int sum = solve("4");
     System.out.println(sum);
-
   }
-  public static int countNumerics(String str){
-    char [] c = str.toCharArray();
+  public static int countNumerics(String str) {
+    char[] c = str.toCharArray();
     int count = 0;
     for (int i = 0; i < c.length; i++) {
-      if (Character.isDigit(c[i])){
+      if (Character.isDigit(c[i])) {
         count++;
       }
     }
-  return count;}
+    return count;
+  }
 
-
-  public static int solve(String str){
+  public static int solve(String str) {
     char[] c = str.toCharArray();
     System.out.println(Arrays.toString(c));
     int sum = c[0];
+
     System.out.println(c);
-    for (int i = 2; i < c.length; i+=2) {
-      if (c[i] == '+'){
-      sum += Character.getNumericValue(c[i+2]);
+    for (int i = 2; i < c.length; i += 2) {
+      if (c[i] == '+') {
+        sum += Character.getNumericValue(c[i + 2]);
       }
-      if (c[i]== '-'){
-        sum -= Character.getNumericValue(c[i+2]);
+      if (c[i] == '-') {
+        sum -= Character.getNumericValue(c[i + 2]);
       }
     }
     System.out.println(sum);
 
-  return sum;}
+    return sum;
+  }
 }
